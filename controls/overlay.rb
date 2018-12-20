a78d75ef22ec5d2294460b6fd10c2d519cbc49d8
Haynes Jr <dhaynes@mitre.org>
2018-12-20 06:51:48 -0500

    adding skeleton control overlay

---
+++ controls/overlay.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8                                                                                              
+include_controls 'microsoft-sql-2014-server-stig-baseline' do
+  control '' do
+    title ''
+    desc 'check', ''
+    desc 'fix', '.'
+  end
+end
+
+
+
